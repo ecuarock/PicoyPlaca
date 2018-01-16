837ebb3c0305527a473eb5828a3981d6ef061129
ecuarock <alexnt05@hotmail.com>
2018-01-16 01:58:49 -0500

    Adding the class to validate Plate
    Creation of class to handle the validation to know if a car can be on
    the road.

--- src/com/picoyplaca/classes/CanBeOnRoad.java
+++ src/com/picoyplaca/classes/CanBeOnRoad.java
@@ -1,5 +1,104 @@
 package com.picoyplaca.classes;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CanBeOnRoad {
 
+	private Vehicle vehicle;
+	private String date;
+	private GregorianCalendar time;
+	private static GregorianCalendar MorningStartHour = new GregorianCalendar(0, 0, 0, 7, 00, 0);
+	private static GregorianCalendar MorningStopHour = new GregorianCalendar(0, 0, 0, 9, 30, 0);
+	private static GregorianCalendar EveningStartHour = new GregorianCalendar(0, 0, 0, 16, 00, 0);
+	private static GregorianCalendar EveningStopHour = new GregorianCalendar(0, 0, 0, 19, 30, 0);
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public GregorianCalendar getTime() {
+		return time;
+	}
+
+	public void setTime(GregorianCalendar time) {
+		this.time = time;
+	}
+
+	public CanBeOnRoad(Vehicle vehicle, String date, GregorianCalendar time) {
+		this.vehicle = vehicle;		
+		this.time = time;
+
+		String RegEx = "[2][0][0-9][0-9][/][0-1][0-9][/][0-3][0-9]\\z";
+		Pattern pattern = Pattern.compile(RegEx);
+		Matcher matcher = pattern.matcher(date);
+		if(matcher.matches())
+		{
+			this.date = date;
+		}
+		else
+		{
+			throw new RuntimeException("Invalid Input Date");
+		}		
+	}
+
+	public boolean Predictor() {		
+		DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		Date inputdate = null;
+		try {
+			inputdate = format.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(inputdate);
+
+		switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+		case 2:
+
+			break;
+
+		case 3:
+
+			break;
+
+		case 4:
+
+			break;
+
+		case 5:
+
+			break;
+
+		case 6:
+
+			break;
+
+		default:
+			return true;
+		}
+
+		System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+
+		return false;
+	}
 }
--- src/com/picoyplaca/tdd/CanBeOnRoadTest.java
+++ src/com/picoyplaca/tdd/CanBeOnRoadTest.java
@@ -2,6 +2,8 @@ package com.picoyplaca.tdd;
 
 import static org.junit.Assert.*;
 
+import java.util.GregorianCalendar;
+
 import org.junit.Test;
 
 import com.picoyplaca.classes.CanBeOnRoad;
@@ -10,8 +12,23 @@ import com.picoyplaca.classes.Vehicle;
 public class CanBeOnRoadTest {
 
 	@Test
-	public void test() {
-		CanBeOnRoad road = new CanBeOnRoad(new Vehicle("pbc-1234"),"2018/01/15","17:00");
+	public void validateConstructorInVehicle() {
+		assertEquals(Vehicle.class, new CanBeOnRoad(new Vehicle("pbc-1234"),"2018/01/15",new GregorianCalendar(0, 0, 0, 17, 30, 0)).getVehicle().getClass());		
+	}
+	
+	@Test
+	public void validateConstructorInDate() {
+		assertEquals(String.class, new CanBeOnRoad(new Vehicle("pbc-1234"),"2018/01/15",new GregorianCalendar(0, 0, 0, 17, 30, 0)).getDate().getClass());		
+	}
+	
+	@Test
+	public void validateConstructorInTime() {
+		assertEquals(GregorianCalendar.class, new CanBeOnRoad(new Vehicle("pbc-1234"),"2018/01/15",new GregorianCalendar(0, 0, 0, 17, 30, 0)).getTime().getClass());		
+	}
+	
+	@Test
+	public void validatePredictor(){
+		assertTrue(new CanBeOnRoad(new Vehicle("pbc-1234"),"2018/01/15",new GregorianCalendar(0, 0, 0, 17, 30, 0)).Predictor());		
 	}
 
 }
